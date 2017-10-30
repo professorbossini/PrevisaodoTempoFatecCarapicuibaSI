e031c1638c3e3f7023c7f7dbaf614e2536d5109e
Rodrigo Bossini <professorbossini@gmail.com>
2017-10-30 15:39:08 -0200

    Falta fazer a requisição ao ws e converter o JSON.

--- app/src/main/java/br/com/bossini/previsaodotempofateccarapicuibasi/MainActivity.java
+++ app/src/main/java/br/com/bossini/previsaodotempofateccarapicuibasi/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Weather> weatherList = new ArrayList<>();
+    private WeatherArrayAdapter weatherArrayAdapter;
+    private ListView weatherListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        weatherListView = (ListView) findViewById(R.id.weatherListView);
+        weatherArrayAdapter = new WeatherArrayAdapter(this, weatherList);
+        weatherListView.setAdapter(weatherArrayAdapter);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
